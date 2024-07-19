061ecff875f0a169bb9fdf26c8bb40726a8720f5
Zoey <zoeykaiser8@gmail.com>
2024-07-19 14:48:38 +0200

    fix: slack issue button, icon rendering twice, coping html code from title

--- scripts/slack-issue-pr-message.js
+++ scripts/slack-issue-pr-message.js
@@ -1,22 +1,29 @@
 // ==UserScript==
-// @name         Sidestream: Github to Slack message shortcut
+// @name         Github Slack Issue Button
 // @namespace    http://tampermonkey.net/
 // @version      1.0.0
 // @description  Share your PRs or Issues via a nicely formatted message via a new 'Slack' button.
-// @author       Zoey Kaiser
-// @match        https://github.com/zoey-kaiser
+// @author       Zoey Kaiser and Damian Gleumes
+// @match        https://github.com/*
 // @grant        GM_setClipboard
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    const GITHUB_PARENT_CONTAINER = '.rgh-conversation-activity-filter-wrapper'
+    const GITHUB_PARENT_STICKY_CONTAINER = '.sticky-content .meta'
+
     const style = document.createElement('style');
     style.textContent = `
-        .my-clickable-svg:hover path {
+        .SlackCopyButton {
+            margin-left: 0.5rem !important;
+        }
+
+        .SlackCopyButton:hover path {
             fill: #2F81F7;
         }
-        .my-clickable-svg {
+        .SlackCopyButton {
             cursor: pointer;
         }
 
@@ -29,18 +36,18 @@
             }
         }
 
-        .my-clickable-svg:active path {
+        .SlackCopyButton:active path {
             fill: green !important;
         }
 
-        .my-clickable-svg.clicked path {
+        .SlackCopyButton.clicked path {
             animation: clickEffect 3.5s;
         }
     `;
     document.head.appendChild(style);
 
     const buttonSVGString = `
-        <svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="octicon octicon-copy my-clickable-svg">
+        <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="octicon octicon-copy SlackCopyButton">
             <path d="M9.33315 2C8.22858 2 7.33328 2.89731 7.33328 4.00388C7.33302 4.26677 7.38454 4.52714 7.4849 4.77012C7.58526 5.0131 7.73249 5.23393 7.9182 5.42001C8.1039 5.60609 8.32444 5.75377 8.56722 5.85462C8.80999 5.95546 9.07026 6.0075 9.33315 6.00777H11.3333V4.00388C11.3338 3.47292 11.1233 2.96353 10.7482 2.58774C10.3731 2.21194 9.86411 2.00053 9.33315 2ZM9.33315 7.34369H3.99984C2.89527 7.34369 1.99997 8.241 1.99997 9.34791C1.99997 10.4545 2.89527 11.3518 3.99984 11.3518H9.33348C10.4377 11.3518 11.3333 10.4545 11.3333 9.34791C11.3333 8.241 10.4377 7.34369 9.33315 7.34369Z"/>
             <path d="M22 9.34791C22 8.241 21.1043 7.34369 19.9998 7.34369C18.8952 7.34369 17.9999 8.241 17.9999 9.34791V11.3518H19.9998C20.5307 11.3513 21.0397 11.1398 21.4148 10.7641C21.7899 10.3883 22.0004 9.87887 22 9.34791ZM16.6667 9.34791V4.00388C16.6671 3.47292 16.4566 2.96353 16.0815 2.58774C15.7064 2.21194 15.1974 2.00053 14.6665 2C13.5619 2 12.6666 2.89731 12.6666 4.00388V9.34757C12.6666 10.4548 13.5619 11.3521 14.6665 11.3521C15.1974 11.3516 15.7064 11.1402 16.0815 10.7644C16.4566 10.3886 16.6671 9.87887 16.6667 9.34791Z"/>
             <path d="M14.6665 22.0395C15.1974 22.039 15.7064 21.8276 16.0815 21.4518C16.4566 21.076 16.6671 20.5666 16.6667 20.0356C16.6671 19.5047 16.4566 18.9953 16.0815 18.6195C15.7064 18.2437 15.1974 18.0323 14.6665 18.0317H12.6666V20.0356C12.6666 21.1422 13.5619 22.0395 14.6665 22.0395ZM14.6665 16.6958H20.0001C21.1043 16.6958 22 15.7985 22 14.6916C22.0004 14.1606 21.7899 13.6512 21.4148 13.2755C21.0397 12.8997 20.5307 12.6882 19.9998 12.6877H14.6665C13.5619 12.6877 12.6666 13.585 12.6666 14.6916C12.6663 14.9545 12.7178 15.2149 12.8182 15.4578C12.9186 15.7008 13.0658 15.9216 13.2515 16.1077C13.4372 16.2938 13.6577 16.4415 13.9005 16.5423C14.1433 16.6432 14.4036 16.6956 14.6665 16.6958Z"/>
@@ -54,7 +61,7 @@
         buttonSVGElement.addEventListener('click', function() {
             this.classList.add('clicked');
 
-            const title = document.querySelector('.js-issue-title').innerHTML;
+            const title = document.title.split(' by')[0];
             const headerNumber = document.querySelector('.gh-header-number').innerHTML;
             const fullURL = new URL(window.location.href);
             const displayURL = `${fullURL.protocol}//${fullURL.host}${fullURL.pathname}`;
@@ -81,16 +88,14 @@
     const observer = new MutationObserver((mutationsList, observer) => {
         for(let mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                // console.log('ensureee!')
-                ensureButtonOnContainer(document.querySelector('.gh-header-meta > *:last-child'));
-                ensureButtonOnContainer(document.querySelector('.sticky-content .meta'));
+                ensureButtonOnContainer(document.querySelector(GITHUB_PARENT_CONTAINER));
+                ensureButtonOnContainer(document.querySelector(GITHUB_PARENT_STICKY_CONTAINER));
                 break;
             }
         }
     });
     observer.observe(document.body, { childList: true, subtree: true });
 
-    ensureButtonOnContainer(document.querySelector('.gh-header-meta > *:last-child'));
-    ensureButtonOnContainer(document.querySelector('.sticky-content .meta'));
-
+    ensureButtonOnContainer(document.querySelector(GITHUB_PARENT_CONTAINER));
+    ensureButtonOnContainer(document.querySelector(GITHUB_PARENT_STICKY_CONTAINER));
 })();
